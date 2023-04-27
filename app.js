cd156274918708683017b6c62e26150d5d8dbf9d
Your Name <muaddakkak34@gmail.com>
2023-04-27 17:37:49 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
+
+
